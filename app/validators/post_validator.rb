a192be3ad53d2e031d5140529f0bf8dff843dc54
Doug Jones <DSJR1284@GMAIL.COM>
2021-03-29 17:26:45 -0400

    commit tree 2af500c7277279c27957b4441fa72ded7f20b80a

---
+++ app/validators/post_validator.rb
@@ -0,0 +1,14 @@
+class PostValidator < ActiveModel::Validator
+
+    def validate(record)
+        
+    end
+
+end 
+
+
+# def clickbait?
+#     unless title && title.match(/Won't Believe |Secret|Top [0-9]+|Guess/)
+#         errors[:title] << "Not Click Bait"
+#     end 
+# end 
